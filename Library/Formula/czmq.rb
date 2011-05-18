0d473c6594965cec99241c434f720faf9326203a
slyphon <slyphon@gmail.com>
2011-05-18 21:19:20 +0000

    czmq formula: the high-level zeromq C wrappers

---
+++ Library/Formula/czmq.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Czmq < Formula
+  url 'http://download.zeromq.org/czmq-1.0.0.tar.gz'
+  homepage 'http://czmq.zeromq.org/page:start'
+  md5 '53e1c4dfcd5b2655210c4d196cbf5eca'
+
+  # depends_on 'cmake'
+  depends_on 'ossp-uuid'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+
+    system "make install"
+  end
+end
