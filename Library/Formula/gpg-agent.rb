8c6c0501f41a000eb24a39bb88cf989ddbc5f2fa
Jonathan D. Simms <slyphon@gmail.com>
2011-10-04 14:20:04 +0000

    minor revision of gpg-agent

--- Library/Formula/gpg-agent.rb
+++ Library/Formula/gpg-agent.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class GpgAgent < Formula
-  url 'ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.0.17.tar.bz2'
+  url 'ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.0.18.tar.bz2'
   homepage 'http://www.gnupg.org/'
-  sha1 '41ef5460417ca0a1131fc730849fe3afd49ad2de'
+  sha1 '5ec2f718760cc3121970a140aeea004b64545c46'
 
   depends_on 'libgpg-error'
   depends_on 'libgcrypt'
